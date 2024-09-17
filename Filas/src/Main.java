0761451f7ee3bf7efea9cb85a93a3ef2e47794e8
fabricio-mm <fabriciomartinsmattos9@gmail.com>
2024-09-17 13:48:35 -0300

    Pilhas

--- Filas/src/Main.java
+++ Filas/src/Main.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Queue;
+import java.util.LinkedList;
+
+
+public class Main{
+    public static void main(String[] args) {
+        //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        //Filas = Estrutura de dados baseada em FIFO. First-in, First-out (Pense numa fila de pessoas)
+        //              Uma coleção para armzenar elementos por ordem de prioridade de processamento
+        //              Uma estrutura de dados Linear
+
+        //              add     = enqueue, offer()
+        //              remove  = dequeue, poll()
+
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // A interface Queue em si não pode ser instanciada diretamente. Precisamos de uma classe concreta para criar um objeto de fila.
+        // A classe LinkedList é uma lista duplamente encadeada, o que significa que cada elemento possui referências para o próximo e o anterior
+        // Obs... A lib Queue herda alguns métodos da lib Collection como o isEmpty() para verificar se a fila está vazia.
+
+        Queue<String> fila = new LinkedList<String>();
+
+        fila.offer("Gumball"); //Head, o primeiro da fila
+        fila.offer("Bob Esponja");
+        fila.offer("Mordecai");
+        fila.offer("Johnny Test"); //Tail, o último da fila
+        System.out.println(fila); //[Gumball, Bob Esponja, Mordecai, Johnny Test]
+
+        //Conferindo o tamanho da fila
+        System.out.println(fila.size()); // Esse método também faz parte da Lib Collction
+
+        //Conferindo se alguma pessoa realmente está na fila
+        System.out.println(fila.contains("Gumball")); //Lib Collction == retorna um valor booleano
+
+        // Método peek(), esse método retorna para nós o primeiro (head) da fila sem o remover
+        System.out.println(fila.peek()); //Gumball
+
+        //Método poll(), remove o primeiro da fila
+        fila.poll(); //Gumball saiu da fila
+        fila.poll(); //Bob Esponja saiu da fila
+
+        //Armazenando quem saiu da fila em uma variável
+        String pessoa = fila.poll();
+        System.out.println(pessoa + " foi atendido. Proximo!");
+    }
 }
--- Pilhas/src/Main.java
+++ Pilhas/src/Main.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args) {
+
+        //Pilhas = É uma estrutura de dados LIFO. Last-in First-out
+        //       Amazena objetos como uma torre vertical
+        //       push() para adcionar um objeto ao topo
+        //       pop() para remover um objeto do topo
+
+        Stack<String> pilha = new Stack<String>(); // Aqui estamos criando uma pilha utilizando o modulo Stack
+        // <> serve para colocarmos o tipo de dado que vamos armazenar na pilha, no nosso caso foi string
+
+        System.out.println(pilha.empty());                                                    //true          empty() confere se a pilha está vazia, no momento ela está
+        pilha.push("Forza");
+        pilha.push("Devil May Cry 3");
+        pilha.push("Minecraft");
+        pilha.push("Skyrim");
+        pilha.push("Elden Ring");
+
+        System.out.println(pilha.empty());                                                     //false         pois acabamos de adicionar objetos à nossa pilha
+        System.out.println(pilha); //[Forza, Devil May Cry 3, Minecraft, Skyrim, Elden Ring]
+
+        pilha.pop(); System.out.println(pilha); // [Minecraft, Skyrim]
+        // não é necessário especificar pq o pop SEMPRE vai tirar o do topo da pilha (o ultimo a ser inserido)
+
+        // Itens na pilha: [Forza, Devil May Cry 3, Minecraft, Skyrim]
+        System.out.println("O objeto no topo é " + pilha.peek());                              // peek()   Isso retorna o objeto no topo da pilha o mantendo nela
+        String myFavGame = pilha.pop();                                                        // Aqui estamos armazenando o objeto que foi retirado em uma variável String
+        System.out.println( "Retirei meu jogo favorito "+ myFavGame + " da pilha para jogar"); // Skyrim
+        System.out.println(pilha);                                                             // [Forza, Devil May Cry 3, Minecraft]
+
+        // Para procurar um objeto usamos a função search(), ela  informa a posição do objeto que estamos procurando.
+        System.out.println(pilha.search("Forza")); // 3
+
+        //Vamos supor que somos a Bethesda e temos 1 milhão de cópias de Fallout76 que não foram vendidas e queremos adcionar isso a nossa pilha
+        for(int adciona = 0; adciona <1000000; adciona++) {
+            pilha.push("Fallout76");
+        }
+        // variavel int adiciona é criada e seu valor é igual a 0 | int adciona = 0;
+        // adiciona vai passar pelo código dentro do for 999999 vezes | adiciona <1000000;
+        // a cada vez que adciona executa o código dentro do for, +1 será agregado ao seu valor | adciona++
+
+        System.out.println(pilha);
+
+        // usabilidades de pilhas
+
+        // 1. Recursos de Desfazer/Refazer em Editores de Texto
+        // 2. Navegação Voltar/Avançar no Histórico do Navegador
+        // 3. Algoritmos de Retrocesso (Backtracking)
+        // 4. Chamada de Funções (Pilha de Chamadas)
+
+    }
 }
